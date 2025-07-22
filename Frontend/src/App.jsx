80a56fce72e969aa7a6d0385a3e1853e3629ee77
Naman041202 <142826344+Naman041202@users.noreply.github.com>
2025-07-23 00:23:48 +0530

    Fix case-sensitive path issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
