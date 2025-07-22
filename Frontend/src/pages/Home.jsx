161958f7c922cb1cf8b1b1290f8023337bd10ee4
Naman041202 <142826344+Naman041202@users.noreply.github.com>
2025-07-23 00:27:44 +0530

    Fix case-sensitive path issue

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../Components/Hero";
 import Features from "../Components/Features";
 import PopularFoods from "../Components/PopularFoods";
-import Testimonials from "../Components/testimonials";
+import Testimonials from "../Components/Testimonials";
 import Footer from "../Components/Footer";
 
 const Home = () => {
